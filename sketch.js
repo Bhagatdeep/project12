3856d11578ec28bfaf508c7f5c10f3a46c5d463e
Bhagatdeep <89076824+Bhagatdeep@users.noreply.github.com>
2021-09-07 08:26:21 -0700

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,26 @@
+var marks = [35,38,42,45,43,34,46,41,48,32];
+var i=0;
+
+
+
+function setup() 
+{
+  createCanvas(400,400);
+  
+ for(i=0;i<marks.length;i++)
+  {
+  
+  if(marks[i]>=35)
+  {console.log(marks[i])
+  }
+  }
+  
+}
+
+function draw() 
+{
+  background(30);
+}
+
+ 
+
